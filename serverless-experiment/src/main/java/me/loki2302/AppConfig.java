caee7f39644ae9f280fb915572ba04d7798a3d14
loki2302 <loki2302@loki2302.me>
2017-10-08 14:10:27 -0400

    Added DynamoDB

--- serverless-experiment/src/main/java/me/loki2302/AppConfig.java
+++ serverless-experiment/src/main/java/me/loki2302/AppConfig.java
@@ -1,5 +1,10 @@
 package me.loki2302;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -14,6 +19,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import java.util.Collections;
 import java.util.Date;
 import java.util.Map;
+import java.util.UUID;
 
 @Configuration
 @ComponentScan
@@ -25,29 +31,60 @@ public class AppConfig {
         return new AppProperties();
     }
 
+    @Bean
+    @Autowired
+    public DynamoDBTableMapper<TodoItem, String, ?> todoItemTableMapper(AppProperties appProperties) {
+        AmazonDynamoDB amazonDynamoDB = AmazonDynamoDBClientBuilder.defaultClient();
+        DynamoDBMapperConfig dynamoDBMapperConfig = DynamoDBMapperConfig.builder()
+                .withTableNameOverride(new DynamoDBMapperConfig.TableNameOverride(appProperties.getTodosTableName()))
+                .build();
+        DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB, dynamoDBMapperConfig);
+        DynamoDBTableMapper<TodoItem, String, ?> todoItemTableMapper =
+                dynamoDBMapper.newTableMapper(TodoItem.class);
+        return todoItemTableMapper;
+    }
+
     @RestController
     @CrossOrigin
     public class DummyController {
         @Autowired
         private AppProperties appProperties;
 
+        @Autowired
+        private DynamoDBTableMapper<TodoItem, String, ?> todoItemTableMapper;
+
         @GetMapping("/test")
         public Map<String, String> index() {
-            String message = "hello there " + new Date() + " " + appProperties.getMessageSuffix();
+            TodoItem todoItem = new TodoItem();
+            todoItem.id = UUID.randomUUID().toString();
+            todoItem.text = String.format("Todo %s", new Date());
+            todoItemTableMapper.save(todoItem);
+
+            int count = todoItemTableMapper.count(new DynamoDBScanExpression());
+            String message = String.format("hello there! %s %s. Count: %d",
+                    new Date(),
+                    appProperties.getMessageSuffix(),
+                    count);
             return Collections.singletonMap("message", message);
         }
     }
 
     @ConfigurationProperties("app")
+    @NoArgsConstructor
+    @Data
     public static class AppProperties {
         private String messageSuffix;
+        private String todosTableName;
+    }
 
-        public String getMessageSuffix() {
-            return messageSuffix;
-        }
+    @DynamoDBTable(tableName = "A DUMMY VALUE TO BE OVERRIDDEN BY CONFIGURATION")
+    @NoArgsConstructor
+    @Data
+    public static class TodoItem {
+        @DynamoDBHashKey
+        private String id;
 
-        public void setMessageSuffix(String messageSuffix) {
-            this.messageSuffix = messageSuffix;
-        }
+        @DynamoDBAttribute
+        private String text;
     }
 }
