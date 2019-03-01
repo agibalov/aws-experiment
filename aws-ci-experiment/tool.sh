801204f52df9cafa56aa09d36a25f5c4ae055d84
agibalov <andrey.agibalov@gmail.com>
2019-03-01 08:09:41 -0500

    Save progress

--- aws-ci-experiment/tool.sh
+++ aws-ci-experiment/tool.sh
@@ -1,111 +1,103 @@
 #!/bin/bash
 
-region=us-east-1
-stackName=AwsCiExperimentStack
-templateBodyFilename=pipeline.yaml
+set -x
 
-appS3BucketName=loki2302-s3-bucket
-dummyCodePipelinePipelineS3BucketName=dummy-codepipeline-pipeline-s3-bucket
+Region=us-east-1
 
 command=$1
+envTag=$2
 
-if [ "$command" == "" ]; then
-  echo "No command specified"
-elif [ "$command" == "create" ]; then
-  gitHubOAuthToken=$2
-  if [ "$gitHubOAuthToken" == "" ]; then
-    echo "Please specify GitHub OAuth token"
-  else
-    echo "CREATING"
+if [[ "${envTag}" == "" ]]; then
+  echo "envTag is not specified"
+  exit 1
+fi
 
-    aws cloudformation create-stack \
-      --stack-name $stackName \
-      --template-body file://$templateBodyFilename \
-      --parameters \
-      ParameterKey=DummyGitHubOAuthToken,ParameterValue=$gitHubOAuthToken \
-      ParameterKey=AppS3BucketName,ParameterValue=$appS3BucketName \
-      ParameterKey=DummyCodePipelinePipelineS3BucketName,ParameterValue=$dummyCodePipelinePipelineS3BucketName \
-      --region $region \
-      --capabilities CAPABILITY_NAMED_IAM
+PipelineStackName=${envTag}-pipeline
+AppStackName=${envTag}-app
 
-    aws cloudformation wait stack-create-complete \
-      --stack-name $stackName \
-      --region $region
-  fi
-
-elif [ "$command" == "update" ]; then
-  gitHubOAuthToken=$2
-  if [ "$gitHubOAuthToken" == "" ]; then
-    echo "Please specify GitHub OAuth token"
-  else
-    echo "UPDATING"
-
-    aws cloudformation update-stack \
-      --stack-name $stackName \
-      --template-body file://$templateBodyFilename \
-      --parameters \
-      --parameters \
-      ParameterKey=DummyGitHubOAuthToken,ParameterValue=$gitHubOAuthToken \
-      ParameterKey=AppS3BucketName,ParameterValue=$appS3BucketName \
-      ParameterKey=DummyCodePipelinePipelineS3BucketName,ParameterValue=$dummyCodePipelinePipelineS3BucketName \
-      --capabilities CAPABILITY_NAMED_IAM \
-      --region $region
-
-    aws cloudformation wait stack-update-complete \
-      --stack-name $stackName \
-      --region $region
-  fi
-
-elif [ "$command" == "delete" ]; then
-  echo "DELETING"
-
-  # CloudFormation can only delete empty S3 buckets
-  aws s3 rm s3://$appS3BucketName/ --recursive
-  aws s3 rm s3://$dummyCodePipelinePipelineS3BucketName/ --recursive
-
-  # CloudFormation will fail to delete the stack,
-  # because its default role is going to be deleted at some point
-  # during the stack destruction.
-  # To avoid this, I temporarily create a new admin role
-  # and use it to delete the stack
-  adminRoleName=DummyAdminRole
-  adminAccessPolicyArn=arn:aws:iam::aws:policy/AdministratorAccess
-
-  aws iam create-role \
-    --role-name $adminRoleName \
-    --assume-role-policy-document file://cloudformation-assume.json
-
-  aws iam attach-role-policy \
-    --role-name $adminRoleName \
-    --policy-arn "$adminAccessPolicyArn"
-
-  roleArn=`\
-  aws iam get-role --role-name $adminRoleName | \
-  python -c "import json,sys;print json.load(sys.stdin)['Role']['Arn']"`
-
-  echo "Temporary admin role ARN is $roleArn"
-
-  # Looks like create-role and attach-role-policy operations do something
-  # after finishing. Give them 10 seconds to actually finish whatever
-  # they do there.
-  sleep 10
-
+undeploy_stack() {
+  local stackName=$1
   aws cloudformation delete-stack \
-    --role-arn "$roleArn" \
-    --stack-name $stackName \
-    --region $region
+    --stack-name ${stackName} \
+    --region ${Region}
 
   aws cloudformation wait stack-delete-complete \
-    --stack-name $stackName \
-    --region $region
+    --stack-name ${stackName} \
+    --region ${Region}
+}
 
-  aws iam detach-role-policy \
-    --role-name $adminRoleName \
-    --policy-arn "$adminAccessPolicyArn"
+get_stack_output() {
+  local stackName=$1
+  local outputName=$2
+  aws cloudformation describe-stacks \
+    --stack-name ${stackName} \
+    --query 'Stacks[0].Outputs[?OutputKey==`'${outputName}'`].OutputValue' \
+    --output text \
+    --region ${Region}
+}
 
-  aws iam delete-role \
-    --role-name $adminRoleName
+get_stack_parameter() {
+  local stackName=$1
+  local parameterName=$2
+  aws cloudformation describe-stacks \
+    --stack-name ${stackName} \
+    --query 'Stacks[0].Parameters[?ParameterKey==`'${parameterName}'`].ParameterValue' \
+    --output text \
+    --region ${Region}
+}
 
-else
-  echo "Unknown command '$command'"
+if [[ "${command}" == "deploy-pipeline" ]]; then
+  gitHubRepositoryOwner=$3
+  gitHubRepositoryName=$4
+  gitHubRepositoryBranchName=$5
+  gitHubRepositoryOAuthToken=$6
+
+  if [[ "${gitHubRepositoryOAuthToken}" == "" ]]; then
+    gitHubRepositoryOAuthToken=$(get_stack_parameter ${PipelineStackName} "GitHubRepositoryOAuthToken")
+  fi
+
+  if [[ "${gitHubRepositoryOAuthToken}" == "" ]]; then
+    echo "gitHubRepositoryOAuthToken is not set"
+    exit 1
+  fi
+
+  aws cloudformation deploy \
+    --template-file pipeline.yml \
+    --stack-name ${PipelineStackName} \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides \
+    EnvTag=${envTag} \
+    GitHubRepositoryOwnerName=${gitHubRepositoryOwner} \
+    GitHubRepositoryName=${gitHubRepositoryName} \
+    GitHubRepositoryBranchName=${gitHubRepositoryBranchName} \
+    GitHubRepositoryOAuthToken=${gitHubRepositoryOAuthToken} \
+    --region ${Region}
+
+elif [[ "${command}" == "undeploy-pipeline" ]]; then
+  pipelineArtifactStoreBucketName=$(get_stack_output ${PipelineStackName} "PipelineArtifactStoreBucketName")
+  aws s3 rm s3://${pipelineArtifactStoreBucketName}/ --recursive
+
+  undeploy_stack ${PipelineStackName}
+
+elif [[ "${command}" == "start-pipeline" ]]; then
+  pipelineName=$(get_stack_output ${PipelineStackName} "PipelineName")
+  aws codepipeline start-pipeline-execution \
+    --name ${pipelineName} \
+    --region ${Region}
+
+elif [[ "${command}" == "deploy-app" ]]; then
+  aws cloudformation deploy \
+    --template-file app.yml \
+    --stack-name ${AppStackName} \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides \
+    EnvTag=${envTag} \
+    --region ${Region}
+
+elif [[ "${command}" == "undeploy-app" ]]; then
+  undeploy_stack ${AppStackName}
+
+elif [[ "${command}" == "" ]]; then
+  echo "No command specified"
+
 fi
