3fdb035625ede884cdb1c9adbe7f5c362b7b937d
agibalov <andrey.agibalov@gmail.com>
2020-04-05 14:30:37 -0400

    LABS-1370: add AWS S3 presigned URL file upload test

--- aws-s3-experiment/src/test/java/io/agibalov/PresignedUrlTest.java
+++ aws-s3-experiment/src/test/java/io/agibalov/PresignedUrlTest.java
@@ -2,17 +2,24 @@ package io.agibalov;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.*;
+import com.amazonaws.util.IOUtils;
 import org.junit.Rule;
 import org.junit.Test;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
 import java.sql.Date;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,6 +38,7 @@ public class PresignedUrlTest {
 
         amazonS3.putObject(TEST_BUCKET_NAME, "1.txt", "hello there");
         URL url = amazonS3.generatePresignedUrl(new GeneratePresignedUrlRequest(TEST_BUCKET_NAME, "1.txt")
+                .withMethod(com.amazonaws.HttpMethod.GET)
                 .withResponseHeaders(new ResponseHeaderOverrides()
                         .withContentDisposition(String.format("attachment; filename=custom123.txt")))
                 .withExpiration(Date.from(Instant.now().plus(1, ChronoUnit.HOURS))));
@@ -50,6 +58,45 @@ public class PresignedUrlTest {
             assertEquals("custom123.txt", responseEntity.getHeaders().getContentDisposition().getFilename());
         }
         assertEquals("hello there", responseEntity.getBody());
+
+        ObjectListing objectListing = amazonS3.listObjects(TEST_BUCKET_NAME);
+        List<S3ObjectSummary> objectSummaries = objectListing.getObjectSummaries();
+        List<DeleteObjectsRequest.KeyVersion> keys = objectSummaries.stream()
+                .map(s -> new DeleteObjectsRequest.KeyVersion(s.getKey()))
+                .collect(Collectors.toList());
+        amazonS3.deleteObjects(new DeleteObjectsRequest(TEST_BUCKET_NAME)
+                .withKeys(keys));
+
+        amazonS3.deleteBucket(TEST_BUCKET_NAME);
+    }
+
+    @Test
+    public void canGenerateAPresignedUploadLink() throws IOException {
+        AmazonS3 amazonS3 = amazonS3Provider.getAmazonS3();
+        amazonS3.createBucket(TEST_BUCKET_NAME);
+
+        URL url = amazonS3.generatePresignedUrl(new GeneratePresignedUrlRequest(TEST_BUCKET_NAME, "1.txt")
+                .withMethod(com.amazonaws.HttpMethod.PUT)
+                .withExpiration(Date.from(Instant.now().plus(1, ChronoUnit.HOURS))));
+
+        String urlString = url.toString();
+        System.out.printf("URL string: %s\n", urlString);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Note: have to manually construct URL because RestTemplate does the weird things internally:
+        // https://stackoverflow.com/a/47311387/852604
+        ResponseEntity<Void> responseEntity = restTemplate.exchange(
+                new RequestEntity<>(
+                        "hello world!",
+                        HttpMethod.PUT,
+                        URI.create(urlString)),
+                Void.class);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+
+        S3Object s3Object = amazonS3.getObject(TEST_BUCKET_NAME, "1.txt");
+        String contentString = IOUtils.toString(s3Object.getObjectContent());
+        assertEquals("hello world!", contentString);
 
         ObjectListing objectListing = amazonS3.listObjects(TEST_BUCKET_NAME);
         List<S3ObjectSummary> objectSummaries = objectListing.getObjectSummaries();
