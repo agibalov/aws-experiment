2725d888f84775bd09068c927739ff4d9b3fdae9
agibalov <andrey.agibalov@gmail.com>
2021-07-25 14:38:22 -0400

    LABS-1465: save progress

--- xray-experiment/src/main/java/io/agibalov/App.java
+++ xray-experiment/src/main/java/io/agibalov/App.java
@@ -1,7 +1,9 @@
 package io.agibalov;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +13,7 @@ import java.time.Instant;
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        new SpringApplicationBuilder(App.class).bannerMode(Banner.Mode.OFF).run(args);
     }
 
     @RestController
