03503e6552a7808b828936ed745c3ff53f1b35c7
agibalov <andrey.agibalov@gmail.com>
2021-11-06 19:16:40 -0400

    LABS-1465: save progress

--- xray-experiment/src/main/java/io/agibalov/App.java
+++ xray-experiment/src/main/java/io/agibalov/App.java
@@ -1,21 +1,28 @@
 package io.agibalov;
 
 import com.amazonaws.xray.AWSXRay;
-import com.amazonaws.xray.entities.Segment;
 import com.amazonaws.xray.entities.Subsegment;
+import com.amazonaws.xray.spring.aop.AbstractXRayInterceptor;
+import com.amazonaws.xray.spring.aop.XRayEnabled;
 import lombok.SneakyThrows;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.Instant;
+import java.util.Map;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class App {
     public static void main(String[] args) {
         new SpringApplicationBuilder(App.class).bannerMode(Banner.Mode.OFF).run(args);
@@ -31,6 +38,27 @@ public class App {
         return new DummyController(dummyService);
     }
 
+    @Bean
+    public XRayInspector xRayInspector() {
+        return new XRayInspector();
+    }
+
+    @Aspect
+    public class XRayInspector extends AbstractXRayInterceptor {
+        @Override
+        protected Map<String, Map<String, Object>> generateMetadata(
+                ProceedingJoinPoint proceedingJoinPoint,
+                Subsegment subsegment) {
+
+            return super.generateMetadata(proceedingJoinPoint, subsegment);
+        }
+
+        @Override
+        @Pointcut("@within(com.amazonaws.xray.spring.aop.XRayEnabled)")
+        public void xrayEnabledClasses() {
+        }
+    }
+
     @RequestMapping
     public static class DummyController {
         private final DummyService dummyService;
@@ -41,9 +69,8 @@ public class App {
 
         @GetMapping
         public ResponseEntity<?> hello() {
-            try (Subsegment subsegment = AWSXRay.beginSubsegment("DummyControllerSubsegment")) {
+            try (Subsegment subsegment = AWSXRay.beginSubsegment("DummyControllerHandmade1")) {
                 subsegment.putMetadata("omg", "qwerty");
-
                 dummyService.doSomething();
                 return ResponseEntity.ok(String.format("Hello %s", Instant.now()));
             } finally {
@@ -52,6 +79,7 @@ public class App {
         }
     }
 
+    @XRayEnabled
     public static class DummyService {
         private final JdbcTemplate jdbcTemplate;
 
@@ -61,11 +89,15 @@ public class App {
 
         @SneakyThrows
         public void doSomething() {
-            try (Subsegment subsegment = AWSXRay.beginSubsegment("DummyServiceSubsegment1")) {
+            try (Subsegment subsegment = AWSXRay.beginSubsegment("DummyServiceHandmade1")) {
                 jdbcTemplate.queryForObject("select 1 + 1", Integer.class);
             }
 
-            try (Subsegment subsegment = AWSXRay.beginSubsegment("DummyServiceSubsegment2")) {
+            try (Subsegment subsegment = AWSXRay.beginSubsegment("DummyServiceHandmade2")) {
+                jdbcTemplate.queryForObject("select 2 + 2", Integer.class);
+            }
+
+            try (Subsegment subsegment = AWSXRay.beginSubsegment("DummyServiceHandmade3")) {
                 subsegment.putMetadata("hello", "world");
                 Thread.sleep(10);
             }
