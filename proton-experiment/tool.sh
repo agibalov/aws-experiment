23d499c8ce61d81e0a05cb6917b2ce794f6c766b
agibalov <andrey.agibalov@gmail.com>
2021-12-13 18:13:02 -0500

    LABS-1496: save progress

---
+++ proton-experiment/tool.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+set -x
+
+Region=us-east-1
+BaseStackName=proton-experiment-base
+DummyTemplateName=template1
+
+command=$1
+
+undeploy_stack() {
+  local stackName=$1
+  aws cloudformation delete-stack \
+    --stack-name ${stackName} \
+    --region ${Region}
+
+  aws cloudformation wait stack-delete-complete \
+    --stack-name ${stackName} \
+    --region ${Region}
+}
+
+get_stack_output() {
+  local stackName=$1
+  local outputName=$2
+  aws cloudformation describe-stacks \
+    --stack-name ${stackName} \
+    --query 'Stacks[0].Outputs[?OutputKey==`'${outputName}'`].OutputValue' \
+    --output text \
+    --region ${Region}
+}
+
+if [[ "${command}" == "deploy-base" ]]; then
+  aws cloudformation deploy \
+    --template-file base.yaml \
+    --stack-name ${BaseStackName} \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --region ${Region}
+
+elif [[ "${command}" == "undeploy-base" ]]; then
+  undeploy_stack ${BaseStackName}
+
+elif [[ "${command}" == "create-environment-template" ]]; then
+  aws proton create-environment-template \
+    --name ${DummyTemplateName} \
+    --region ${Region}
+
+elif [[ "${command}" == "delete-environment-template" ]]; then
+  aws proton create-environment-template \
+    --name ${DummyTemplateName} \
+    --region ${Region}
+
+elif [[ "${command}" == "create-environment-template-version" ]]; then
+  timestamp=$(date '+%s')
+  bundleFilename=${timestamp}.tar.gz
+  tar -zcvf ${bundleFilename} environment/
+
+  templatesBucketName=$(get_stack_output "${BaseStackName}" "BucketName")
+  aws s3 cp ${bundleFilename} s3://${templatesBucketName}/${bundleFilename}
+  aws proton create-environment-template-version \
+    --template-name ${DummyTemplateName} \
+    --source s3=\{bucket=${templatesBucketName},key=${bundleFilename}\} \
+    --region ${Region}
+
+  rm ${bundleFilename}
+
+elif [[ "${command}" == "" ]]; then
+  echo "No command specified"
+else
+  echo "Unknown command ${command}"
+fi
