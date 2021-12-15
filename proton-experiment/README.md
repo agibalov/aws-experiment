d6dc9694eac5c8de59143f8c86742f41abef27c1
agibalov <andrey.agibalov@gmail.com>
2021-12-15 15:25:55 -0500

    LABS-1496: save progress

--- proton-experiment/README.md
+++ proton-experiment/README.md
@@ -7,7 +7,10 @@ The AWS Proton hello world.
 * `./tool.sh deploy-base` (and `./tool.sh undeploy-base`) to deploy and undeploy the CloudFormation stack with resources necessary for Proton to run.
 * `./tool.sh create-environment-template` (and `./tool.sh create-environment-template`) to create and delete the environment template.
 * `./tool.sh create-environment-template-version` to create the environment template version.
-* `envName=<ENVNAME> majorVersion=<MAJORVERSION> minorVersion=<MINORVERSION> ./tool.sh create-environment` to create the environment. `<ENVNAME>` is environment name, like `dev`. `<MAJORVERSION>` and `<MINORVERSION>` are the environment template version to be used for this environment (see the output of `./tool.sh create-environment-template-version`)
+* `envName=<ENVNAME> majorVersion=<MAJORVERSION> minorVersion=<MINORVERSION> ./tool.sh create-environment` to create (and destroy) the environment. `<ENVNAME>` is environment name, like `dev`. `<MAJORVERSION>` and `<MINORVERSION>` are the environment template version to be used for this environment (see the output of `./tool.sh create-environment-template-version`)
+* `./tool.sh create-service-template` (and `./tool.sh delete-service-template`) to create and delete the service template.
+* `envName=<ENVNAME> serviceName=<SERVICENAME> majorVersion=<MAJORVERSION> ./tool.sh create-service` (and `serviceName=<SERVICENAME> ./tool.sh delete-service`) to create and destroy services. `<ENVNAME>` is environment name, like `dev`. `<SERVICENAME>` is a service name, like `app1`. `<MAJORVERSION>` is the major version of the service template.
+* TODO: create-service, delete-service
 
 ## Notes
 
--- proton-experiment/tool.sh
+++ proton-experiment/tool.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-set -x
+set -ex
 
 Region=us-east-1
 BaseStackName=proton-experiment-base
 EnvironmentTemplateName=env1
+ServiceTemplateName=service1
 
 command=$1
 
@@ -34,6 +35,11 @@ if [[ "${command}" == "deploy-base" ]]; then
     --template-file base.yaml \
     --stack-name ${BaseStackName} \
     --capabilities CAPABILITY_NAMED_IAM \
+    --region ${Region}
+
+  pipelineServiceRoleArn=$(get_stack_output ${BaseStackName} "PipelineServiceRoleArn")
+  aws proton update-account-settings \
+    --pipeline-service-role-arn ${pipelineServiceRoleArn} \
     --region ${Region}
 
 elif [[ "${command}" == "undeploy-base" ]]; then
@@ -101,14 +107,88 @@ elif [[ "${command}" == "create-environment" ]]; then
   aws proton wait environment-deployed \
     --name ${envName} \
     --region ${Region}
-  aws proton get-environment \
-    --name ${envName} \
-    --region ${Region}
 
 elif [[ "${command}" == "delete-environment" ]]; then
   envName=${envName:?not set or empty}
   aws proton delete-environment \
     --name ${envName} \
+    --region ${Region}
+
+elif [[ "${command}" == "create-service-template" ]]; then
+  aws proton create-service-template \
+    --name ${ServiceTemplateName} \
+    --region ${Region}
+
+elif [[ "${command}" == "delete-service-template" ]]; then
+  aws proton delete-service-template \
+    --name ${ServiceTemplateName} \
+    --region ${Region}
+
+elif [[ "${command}" == "create-service-template-version" ]]; then
+  envMajorVersion=${envMajorVersion:?not set or empty}
+
+  timestamp=$(date '+%s')
+  bundleFilename=${timestamp}.tar.gz
+  tar -zcvf ${bundleFilename} service/
+
+  templatesBucketName=$(get_stack_output "${BaseStackName}" "BucketName")
+  aws s3 cp ${bundleFilename} s3://${templatesBucketName}/${bundleFilename}
+  serviceTemplateVersionJson=$(aws proton create-service-template-version \
+    --compatible-environment-templates \
+    '[{"templateName":"'${EnvironmentTemplateName}'","majorVersion":"'${envMajorVersion}'"}]' \
+    --template-name ${ServiceTemplateName} \
+    --source s3=\{bucket=${templatesBucketName},key=${bundleFilename}\} \
+    --region ${Region})
+
+  rm ${bundleFilename}
+
+  majorVersion=$(echo ${serviceTemplateVersionJson} | jq '.serviceTemplateVersion.majorVersion|tonumber')
+  minorVersion=$(echo ${serviceTemplateVersionJson} | jq '.serviceTemplateVersion.minorVersion|tonumber')
+
+  aws proton wait service-template-version-registered \
+    --major-version ${majorVersion} \
+    --minor-version ${minorVersion} \
+    --template-name ${ServiceTemplateName} \
+    --region ${Region}
+
+  aws proton update-service-template-version \
+    --major-version ${majorVersion} \
+    --minor-version ${minorVersion} \
+    --template-name ${ServiceTemplateName} \
+    --status PUBLISHED \
+    --region ${Region}
+
+  echo "Service template version: ${majorVersion}.${minorVersion}"
+
+elif [[ "${command}" == "create-service" ]]; then
+  envName=${envName:?not set or empty}
+  serviceName=${serviceName:?not set or empty}
+  majorVersion=${majorVersion:?not set or empty}
+
+  aws proton create-service \
+    --name ${serviceName} \
+    --spec "{ proton: ServiceSpec, pipeline: { hello: qqq }, instances: [ { name: inst1, environment: ${envName}, spec: { hello: world } } ] }" \
+    --template-name ${ServiceTemplateName} \
+    --template-major-version ${majorVersion} \
+    --repository-connection-arn "arn:aws:codestar-connections:us-east-1:433819845724:connection/3986b1a7-2213-4f23-88c0-858b6e7a6b9d" \
+    --repository-id "agibalov/aws-experiment" \
+    --branch "labs-1496" \
+    --region ${Region}
+
+  aws proton wait service-created \
+    --name ${serviceName} \
+    --region ${Region}
+
+  # TODO: service-instance-deployed, service-pipeline-deployed?
+
+elif [[ "${command}" == "delete-service" ]]; then
+  serviceName=${serviceName:?not set or empty}
+  aws proton delete-service \
+    --name ${serviceName} \
+    --region ${Region}
+
+  aws proton wait service-deleted \
+    --name ${serviceName} \
     --region ${Region}
 
 elif [[ "${command}" == "" ]]; then
